c37e5041c1ee1bd5153c95318ab1491e00f2368f
Valentrix <naufalahmad333@gmail.com>
2020-04-22 14:29:47 +0800

    deklarasi variabel dan tipe data

---
+++ modul1/DataTypes.java
@@ -0,0 +1,12 @@
+public class  DataTypes{
+	public static void main(String [] args){
+		String namaDepan = "Ahmad";
+		String namaTengah = "Naufal";
+		String namaBelakang = "Alfarizi";
+		int usia = 19;
+		int targetTahunKuliah = 4;
+		double ipk = 3.98765432;
+		char nilaiAbjad = 'A';
+		boolean ganteng = true;
+	}
+}
